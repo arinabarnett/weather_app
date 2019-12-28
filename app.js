ec768b8d989149ac4c28f75d2f412adacef3fd80
Arina <arishilyaeva@gmail.com>
2019-12-28 20:38:04 +0300

    added some new features

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -15,8 +20,36 @@ window.addEventListener('load', () => {
                     return response.json()
                 })
                 .then(data => {
-                    console.log(data);
+                    const {temperature, summary, icon} = data.currently;
+
+                    // Set DOM elements from the API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+
+                    // Celcius Formula 
+                    let celcius = (temperature - 32)*(5/9);
+
+                    //Set Icon
+                    setIcons(icon, document.querySelector('.icon'));
+
+                    //Change temperature to Celsius/Farenheit
+                    temperatureSection.addEventListener('click', () => {
+                        if(temperatureSpan.textContent === "F"){
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celcius);
+                        } else {
+                            temperatureSpan.textContent = "F";
+                            temperatureDegree.textContent = temperature;
+                        }
+                    });
                 });
         });
     } 
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
 });
